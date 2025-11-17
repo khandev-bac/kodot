c4a9c43919721ef59b7fb01da276ffd990d42cba
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-11-17 21:38:05 +0530

    add colors

---
+++ lib/contants/Colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class Colors {
+  Colors._();
+  static Color customBlack = Color(0xFF000000);
+  static Color customWhite = Color(0xFFFFFFFF);
+  static Color customRed = Color(0xFFFF0000);
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:kodot/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text("Hello"))),
+    );
+  }
+}
