59e7a29c3e68a6418b4a73fa27fd38f091f42706
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-11-18 21:26:49 +0530

    add logout btn

--- lib/screens/features/ProfileScreen.dart
+++ lib/screens/features/ProfileScreen.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:kodot/contants/Colors.dart';
+import 'package:kodot/service/AuthService.dart';
+import 'package:kodot/widget/CustomButton.dart';
 
 class ProfilSreen extends StatelessWidget {
   const ProfilSreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Authservice authservice = Authservice();
     return Scaffold(
-      body: Center(
-        child: Text(
-          "ProfilSreen",
-          style: TextStyle(
-            color: AppColors.customWhite,
-            fontFamily: "Jost",
-            fontSize: 18,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "ProfilSreen",
+                style: TextStyle(
+                  color: AppColors.customWhite,
+                  fontFamily: "Jost",
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Custombutton(
+                text: "Logout",
+                onTap: () async {
+                  authservice.signOut();
+                },
+              ),
+            ],
           ),
         ),
       ),
